abfa80ec4400c792af8cb7efc0a383394961530d
Harshdeepsinh <87258918+Harshdeepsinh@users.noreply.github.com>
2023-09-03 17:13:54 +0530

    test

--- android/app/src/main/kotlin/com/async/socialbennett/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.async.socialbennett
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/async/socialbennett/orev_ecommerce_app_main/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.async.socialbennett.orev_ecommerce_app_main
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
